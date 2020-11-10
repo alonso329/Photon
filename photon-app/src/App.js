ba1d9f41ba81847bf83ac55f96af4f0a70f16155
Ari Sepulveda <asepulveda1@ucol.mx>
2020-11-09 22:11:49 -0600

    NavBar (Avances)

--- photon-app/src/App.js
+++ photon-app/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-nav">
+        <nav className="navBar">
+          <ul class="menu">
+            <li class="logo">
+              <a href="#">Photon</a>
+            </li>
+            <li class="item">
+              <a href="#">
+                Categorias <FontAwesomeIcon icon={faAngleDown} />
+              </a>
+              <ul class="submenu">
+                <li class="subitem">
+                  <a href="#">Item 1</a>
+                </li>
+                <li class="subitem">
+                  <a href="#">Item 2</a>
+                </li>
+                <li class="subitem">
+                  <a href="#">Item 3</a>
+                </li>
+              </ul>
+            </li>
+            <li class="item">
+              <a href="#">Season Pass</a>
+            </li>
+            <li class="item">
+              <a href="#">Torneos</a>
+            </li>
+            <li class="item">
+              <a href="#">Eventos</a>
+            </li>
+            <li class="item">
+              <a href="#">Ayuda</a>
+            </li>
+            <li class="item button">
+              <a href="#">Registrarse</a>
+            </li>
+            <li class="item button secondary">
+              <a href="#">Ingresar</a>
+            </li>
+            <li class="menuBars">
+              <a href="#">
+                <FontAwesomeIcon icon={faBars} />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
