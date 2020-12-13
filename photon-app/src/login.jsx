36cdd9a0faa1eddc904a2b44dee50c41dcecc7dd
alonso329 <asepulveda1@ucol.mx>
2020-12-13 17:18:46 -0600

    add firebase and fixing login styles

--- photon-app/src/login.jsx
+++ photon-app/src/login.jsx
@@ -1,53 +1,94 @@
+import { useState } from "react";
 import {
   Box,
   Button,
   FormControl,
   FormLabel,
   Heading,
-  HStack,
   Input,
   Text,
   VStack,
-  Grid,
-  GridItem,
+  extendTheme,
 } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
+import "firebase/auth";
+import { useFirebaseApp } from "reactfire";
 
 export default function Login() {
+  const { email, setEmail } = useState("");
+  const { password, setPassword } = useState("");
+
+  const firebase = useFirebaseApp();
+
+  const theme = extendTheme({
+    styles: {
+      global: {
+        body: {
+          bg: "cyan.900",
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          pt: "10%",
+        },
+      },
+    },
+  });
+
+  const submit = async (email, password) => {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        // Signed in
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+  };
+
   return (
-    <ChakraProvider>
-      <Grid
-        templateColumns="repeat(3, 1fr)"
-        bg="blue.400"
+    <ChakraProvider theme={theme}>
+      <Box
+        align="center"
+        as="form"
+        bgColor="white"
+        p={5}
+        borderRadius={10}
+        w={350}
       >
-        <GridItem colStart={2} rowStart={2}>
-          <Box align="center" as="form" bgColor="white" p={5} borderRadius={10} maxW="200px">
-            <VStack spacing={5}>
-              <Heading size="lg">Iniciar Sesión</Heading>
-              <FormControl id="email">
-                <FormLabel>Correo Electrónico</FormLabel>
-                <Input type="email" placeholder="Correo Electronico" />
-              </FormControl>
-              <FormControl id="password">
-                <FormLabel>Contraseña </FormLabel>
-                <Input type="password" placeholder="Contraseña" />
-              </FormControl>
-              <Button
-                size="md"
-                isFullWidth="true"
-                variant="solid"
-                colorScheme="teal"
-              >
-                INICIAR SESIÓN
-              </Button>
-              <Text>O inicia sesión con</Text>
-                <Button colorScheme="red">
-                  Google
-                </Button>
-            </VStack>
-          </Box>
-        </GridItem>
-      </Grid>
+        <VStack spacing={5}>
+          <Heading size="lg">Iniciar Sesión</Heading>
+          <FormControl id="email">
+            <FormLabel>Correo Electrónico</FormLabel>
+            <Input
+              type="email"
+              placeholder="Correo Electronico"
+              onChange={(ev) => setEmail(ev.target.value)}
+            />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Contraseña </FormLabel>
+            <Input
+              type="password"
+              placeholder="Contraseña"
+              onChange={(ev) => setPassword(ev.target.value)}
+            />
+          </FormControl>
+          <Button
+            size="md"
+            isFullWidth="true"
+            variant="solid"
+            colorScheme="teal"
+            onClick={submit()}
+          >
+            INICIAR SESIÓN
+          </Button>
+          <Text>O inicia sesión con</Text>
+          <Button colorScheme="red">Google</Button>
+        </VStack>
+      </Box>
     </ChakraProvider>
   );
 }
