4c45ccbd30f6c4da1581db1e315549d0c6ff2bba
alonso329 <asepulveda1@ucol.mx>
2020-12-13 23:21:24 -0600

    adding logout button

--- photon-app/src/components/Navbar.jsx
+++ photon-app/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Box,
   HStack,
@@ -9,13 +9,34 @@ import {
   InputGroup,
   InputLeftElement,
   Input,
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogBody,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogContent,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
-import { useUser } from "reactfire";
+import { useUser, useFirebaseApp } from "reactfire";
+import "firebase/firestore";
 
-function Navbar() {
-  const user = useUser();
+export default function Navbar() {
+  const { data: user } = useUser();
+  const [isOpen, setIsOpen] = useState(false);
+  const onClose = () => setIsOpen(false);
+  const cancelRef = React.useRef();
+  const firebase = useFirebaseApp();
+
+  const logout = async () => {
+    await firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setIsOpen(false);
+      });
+  };
+
   return (
     <Box bg="teal.800" w="100%" p={4} color="white">
       <Flex align="center">
@@ -30,17 +51,48 @@ function Navbar() {
           />
           <Input placeholder="Buscar" variant="filled" maxW="25rem" />
         </InputGroup>
-        <HStack spacing={3}>
-          <Link to="/login">
-            <Button colorScheme="teal">Iniciar Sesion</Button>
-          </Link>
-          <Link to="/register">
-            <Button colorScheme="teal">Registrarse</Button>
-          </Link>
-        </HStack>
+        {!user && (
+          <HStack spacing={3}>
+            <Link to="/login">
+              <Button colorScheme="teal">Iniciar Sesion</Button>
+            </Link>
+            <Link to="/register">
+              <Button colorScheme="teal">Registrarse</Button>
+            </Link>
+          </HStack>
+        )}
+        {user && (
+          <Button
+            colorScheme="teal"
+            onClick={() => {
+              setIsOpen(true);
+            }}
+          >
+            Cerrar Sesion
+          </Button>
+        )}
+
+        <AlertDialog
+          motionPreset="slideInBottom"
+          leastDestructiveRef={cancelRef}
+          isOpen={isOpen}
+          isCentered
+        >
+          <AlertDialogOverlay />
+          <AlertDialogContent>
+            <AlertDialogHeader>Cerrar Sesion</AlertDialogHeader>
+            <AlertDialogBody>¿De verdad quieres cerrar sesión?</AlertDialogBody>
+            <AlertDialogFooter>
+              <Button colorScheme="gray" onClick={onClose} mr={4}>
+                No
+              </Button>
+              <Button colorScheme="red" onClick={() => logout()}>
+                Si
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </Flex>
     </Box>
   );
 }
-
-export default Navbar;
